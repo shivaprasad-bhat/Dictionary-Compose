7efe9ba799c46488ec359092d60f7465e984dd29
shivaprasad-bhat <shivaprasad@svbneelmane.me>
2022-07-11 21:50:10 +0530

    feat(): Room and converters

--- app/src/main/java/me/svbneelmane/dictionary/feature_dictionary/data/local/WordIfoDatabase.kt
+++ app/src/main/java/me/svbneelmane/dictionary/feature_dictionary/data/local/WordIfoDatabase.kt
@@ -2,12 +2,14 @@ package me.svbneelmane.dictionary.feature_dictionary.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import me.svbneelmane.dictionary.feature_dictionary.data.local.entity.WordInfoEntity
 
 @Database(
     entities = [WordInfoEntity::class],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class WordIfoDatabase : RoomDatabase() {
     abstract val dao: WordInfoDao
 }
